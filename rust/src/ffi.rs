3a0d9b278798333ab3f6dd9d894c91c9e4994aca
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-07-03 11:42:57 +0100

    Make keccacheck_init work for arbitray number of instances

--- rust/src/ffi.rs
+++ rust/src/ffi.rs
@@ -21,9 +21,10 @@ pub unsafe extern "C" fn keccacheck_init(ptr: *const u8, len: usize) -> *mut c_v
     // SAFETY: Caller must ensure ptr is valid for len bytes
     unsafe {
         assert_eq!(len % 400, 0);
+        let n = len / 400;
         // Gets the words from the data at the pointer
         let data: &[u8] = std::slice::from_raw_parts(ptr, len);
-        
+
         let words: Vec<u64> = (0..len / 8)
             .map(|i| {
                 let mut bytes = [0u8; 8];
@@ -34,28 +35,35 @@ pub unsafe extern "C" fn keccacheck_init(ptr: *const u8, len: usize) -> *mut c_v
 
         // Seperate input/output of the KeccakF function
         let (inp, out) = words.split_at(words.len() / 2);
-        let mut input = [0u64; 25];
-        
-        input.iter_mut().zip(inp.iter()).for_each(|(s, b)| *s = *b);
-        
-        let mut output = [0u64; 25];
-        output.iter_mut().zip(out.iter()).for_each(|(s, b)| *s = *b);
-       
-        // Sanity check on the input and output
-        let mut permuted_input: [u64; 25] = input;
-        keccak::f1600(&mut permuted_input);
-        assert_eq!(permuted_input, output);
 
-        let mut state = KeccakRoundState::at_round(&input, 0);
-        let mut state_data: Vec<u64> = Vec::with_capacity(600);
+        let mut input = vec![0u64; 25 * n];
+        input.iter_mut().zip(inp.iter()).for_each(|(s, b)| *s = *b);
+       
+
+        let mut output = vec![0u64; 25 * n];
+        output.iter_mut().zip(out.iter()).for_each(|(s, b)| *s = *b);
         
-        for _ in 0..23{
-            state_data.extend_from_slice(state.iota.as_slice());
-            state = state.next();
-           
+        // Sanity check on the println!("{:?}",input);input and output
+        for i in 0..n {
+            let mut input_i = [0u64; 25];
+            input_i.clone_from_slice(&input[25 * i..25 * i + 25]);
+            let output_i = &output[25 * i..25 * i + 25];
+            keccak::f1600(&mut input_i);
+            assert_eq!(input_i, output_i);
         }
-        state_data.extend_from_slice(state.iota.as_slice());
+        let mut state_data: Vec<u64> = Vec::with_capacity(600 * n);
+
+        for i in 0..n {
+            let input_i = &mut input[25 * i..25 * i + 25]; 
+              
+            let mut state = KeccakRoundState::at_round(&input_i, 0);
         
+            for _ in 0..23 {
+                state_data.extend_from_slice(state.iota.as_slice());
+                state = state.next();
+            }
+            state_data.extend_from_slice(state.iota.as_slice());
+    }
         let instance = KeccakInstance { data: state_data };
 
         Box::into_raw(Box::new(instance)) as *mut c_void
@@ -86,50 +94,53 @@ pub unsafe extern "C" fn keccacheck_free(ptr: *mut c_void) {
 mod tests {
     use std::ffi::c_void;
 
-    use crate::{keccacheck_init, KeccakInstance};
+    use crate::{KeccakInstance, keccacheck_init};
 
     #[test]
     fn test_keccacheck_init() {
         // Run Keccacheck init and check that the
         // last round in the buffer is consistent with the expected output
-        let  output = [
-                0xF1258F7940E1DDE7,
-                0x84D5CCF933C0478A,
-                0xD598261EA65AA9EE,
-                0xBD1547306F80494D,
-                0x8B284E056253D057,
-                0xFF97A42D7F8E6FD4,
-                0x90FEE5A0A44647C4,
-                0x8C5BDA0CD6192E76,
-                0xAD30A6F71B19059C,
-                0x30935AB7D08FFC64,
-                0xEB5AA93F2317D635,
-                0xA9A6E6260D712103,
-                0x81A57C16DBCF555F,
-                0x43B831CD0347C826,
-                0x01F22F1A11A5569F,
-                0x05E5635A21D9AE61,
-                0x64BEFEF28CC970F2,
-                0x613670957BC46611,
-                0xB87C5A554FD00ECB,
-                0x8C3EE88A1CCF32C8,
-                0x940C7922AE3A2614,
-                0x1841F924A2C509E4,
-                0x16F53526E70465C2,
-                0x75F644E97F30A13B,
-                0xEAF1FF7B5CECA249,
-            ];
-            let output = output.map(u64::swap_bytes);
-            let mut data = [0u64;50];
-            data[25..].copy_from_slice(&output);
-            let ptr : *const [u64] = &data;
+        let output = [
+            0xF1258F7940E1DDE7,
+            0x84D5CCF933C0478A,
+            0xD598261EA65AA9EE,
+            0xBD1547306F80494D,
+            0x8B284E056253D057,
+            0xFF97A42D7F8E6FD4,
+            0x90FEE5A0A44647C4,
+            0x8C5BDA0CD6192E76,
+            0xAD30A6F71B19059C,
+            0x30935AB7D08FFC64,
+            0xEB5AA93F2317D635,
+            0xA9A6E6260D712103,
+            0x81A57C16DBCF555F,
+            0x43B831CD0347C826,
+            0x01F22F1A11A5569F,
+            0x05E5635A21D9AE61,
+            0x64BEFEF28CC970F2,
+            0x613670957BC46611,
+            0xB87C5A554FD00ECB,
+            0x8C3EE88A1CCF32C8,
+            0x940C7922AE3A2614,
+            0x1841F924A2C509E4,
+            0x16F53526E70465C2,
+            0x75F644E97F30A13B,
+            0xEAF1FF7B5CECA249,
+        ];
+        const N : usize = 16;
+        let output = output.map(u64::swap_bytes);
+        let mut data = [0u64; N * 50];
 
-            let result: *mut c_void = unsafe {
-            keccacheck_init(ptr as *const u8, 400)
-            };
-            let instance =unsafe{ &*(result as *const KeccakInstance)};
-            
-            assert_eq!(output[0].swap_bytes(), instance.data[575]);
-            assert_eq!(output[24].swap_bytes(), instance.data[599])
-         }
+        for i in 0..N { 
+            data[N * 25 + i * 25 ..N * 25 + i * 25 + 25].copy_from_slice(&output);
+        }
+        
+        let ptr: *const [u64] = &data;
+
+        let result: *mut c_void = unsafe { keccacheck_init(ptr as *const u8, N * 400) };
+        let instance = unsafe { &*(result as *const KeccakInstance) };
+
+        assert_eq!(output[0].swap_bytes(), instance.data[575]);
+        assert_eq!(output[24].swap_bytes(), instance.data[599])
+    }
 }
