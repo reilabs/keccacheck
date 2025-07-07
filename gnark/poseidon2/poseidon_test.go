7dad2e72c7ff2d08f1ddc15909e255cc180a1218
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-07-07 12:09:10 +0100

    Add test for permute 3

--- gnark/poseidon2/poseidon_test.go
+++ gnark/poseidon2/poseidon_test.go
@@ -1,6 +1,7 @@
 package poseidon2
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc"
@@ -31,6 +32,56 @@ func (c *doubleCircuit) Define(api frontend.API) error {
 	y := Double(api, c.X)
 	api.AssertIsEqual(y, c.Y)
 	return nil
+}
+
+type Permute3Circuit struct {
+	Input  [3]frontend.Variable `gnark:",public"`
+	Output [3]frontend.Variable `gnark:",public"`
+}
+
+func (c *Permute3Circuit) Define(api frontend.API) error {
+	// Make a copy of Input slice to permute
+	permuted := make([]frontend.Variable, 3)
+	copy(permuted, c.Input[:])
+
+	Permute3(api, permuted) // permute in place
+
+	// Assert permuted equals Output
+	for i := 0; i < 3; i++ {
+		api.AssertIsEqual(permuted[i], c.Output[i])
+	}
+
+	return nil
+}
+
+func TestPermute3(t *testing.T) {
+	assert := test.NewAssert(t)
+	var state [3]frontend.Variable
+	for i := 0; i < 3; i++ {
+		state[i] = frontend.Variable(uint64(i))
+	}
+
+	expectedStrings := [3]string{
+		"5297208644449048816064511434384511824916970985131888684874823260532015509555",
+		"21816030159894113985964609355246484851575571273661473159848781012394295965040",
+		"13940986381491601233448981668101586453321811870310341844570924906201623195336",
+	}
+	var expected [3]frontend.Variable
+	for i := 0; i < 3; i++ {
+		n, ok := new(big.Int).SetString(expectedStrings[i], 10)
+		if !ok {
+			t.Fatalf("failed to parse expected value at index %d", i)
+		}
+		expected[i] = n
+	}
+
+	var circuit Permute3Circuit
+	witness := Permute3Circuit{
+		Input:  state,
+		Output: expected,
+	}
+	assert.ProverSucceeded(&circuit, &witness, test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
+
 }
 
 func TestSbox(t *testing.T) {
