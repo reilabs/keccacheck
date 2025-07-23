31003e7fae604044fe4a2b3d26dfa0f51b094b60
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-07-23 11:21:28 +0100

    Add test for failing keccacheck circuit

--- gnark/keccak_verifier_test.go
+++ gnark/keccak_verifier_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math/big"
+	"math/rand"
 	"testing"
 	"unsafe"
 
@@ -32,7 +33,7 @@ func TestKeccakVerify(t *testing.T) {
 			w := inputs[instance*25+i]
 			for j := 0; j < 64; j++ {
 				bit := w.Bit(j)
-				inputDSized[64*(i*N+instance)+j] = frontend.Variable(bit)
+				inputDSized[64*(i*N+instance)+j] = bit
 			}
 		}
 	}
@@ -48,7 +49,7 @@ func TestKeccakVerify(t *testing.T) {
 			for j := 0; j < 64; j++ {
 				bit := (w >> j) & 1
 				flatIndex := 64*(i*N+instance) + j
-				outputSized[flatIndex] = frontend.Variable(bit)
+				outputSized[flatIndex] = bit
 			}
 		}
 	}
@@ -63,6 +64,65 @@ func TestKeccakVerify(t *testing.T) {
 
 	// Assert the prover succeeds with given backend and curve
 	assert.ProverSucceeded(
+		&circuit,
+		&witness,
+		test.WithCurves(ecc.BN254),
+		test.WithBackends(backend.GROTH16),
+	)
+}
+
+func TestKeccakVerifyFailing(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	solver.RegisterHint(KeccacheckProveHint)
+
+	inputs := make([]*big.Int, 25*N)
+	for i := range inputs {
+		inputs[i] = big.NewInt(int64(i))
+	}
+	var inputDSized [64 * 25 * N]frontend.Variable
+	var inputSized [25 * N]frontend.Variable
+	// TODO: simplify this
+	// inputs are currently instance by instance sequentially
+	// But decomposed inputs are stored round by round
+	for i := 0; i < 25; i++ {
+		for instance := 0; instance < N; instance++ {
+			inputSized[instance*25+i] = inputs[instance*25+i]
+			w := inputs[instance*25+i]
+			for j := 0; j < 64; j++ {
+				bit := w.Bit(j)
+				inputDSized[64*(i*N+instance)+j] = bit
+			}
+		}
+	}
+
+	output_ptr := KeccacheckInit(inputs)
+	words := unsafe.Slice((*uint64)(output_ptr), 600*N)
+
+	var outputSized [64 * 25 * N]frontend.Variable
+	flip_idx := rand.Intn(25 * N * 64)
+	for i := 0; i < 25; i++ {
+		for instance := 0; instance < N; instance++ {
+			w := words[575*N+i*N+instance]
+			for j := 0; j < 64; j++ {
+				bit := (w >> j) & 1
+				flatIndex := 64*(i*N+instance) + j
+				if flatIndex == flip_idx {
+					bit ^= 1
+				}
+				outputSized[flatIndex] = bit
+			}
+		}
+	}
+	circuit := KeccakfCircuit{}
+	witness := KeccakfCircuit{
+		InputD: inputDSized,
+		Input:  inputSized,
+		Output: outputSized,
+	}
+
+	// Assert the prover succeeds with given backend and curve
+	assert.ProverFailed(
 		&circuit,
 		&witness,
 		test.WithCurves(ecc.BN254),
