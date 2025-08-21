7831c14b3250a40da9ea0f081a21ec175d8f2c20
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-08-21 14:18:23 +0200

    Change rust verifier to generate beta as polynomial

--- rust/gkr/src/verifier.rs
+++ rust/gkr/src/verifier.rs
@@ -13,8 +13,8 @@ pub fn verify(num_vars: usize, output: &[u64], input: &[u64], proof: &[Fr], mut 
     let mut verifier = Verifier::new(proof);
     let span = tracing::span!(Level::INFO, "calculate output sum").entered();
     r.iter().for_each(|challenge| verifier.absorb(*challenge));
-    let mut beta = (0..25).map(|_| verifier.generate()).collect::<Vec<_>>();
-
+    let mut beta = vec![Fr::zero(); 25];
+    generate_beta(&mut verifier, &mut beta);
     let expected_sum = (0..25)
         .map(|i| {
             beta[i]
@@ -41,8 +41,8 @@ pub fn verify(num_vars: usize, output: &[u64], input: &[u64], proof: &[Fr], mut 
         );
         if round != 0 {
             sum = Fr::zero();
+            generate_beta(&mut verifier, &mut beta);
             beta.iter_mut().enumerate().for_each(|(i, b)| {
-                *b = verifier.generate();
                 sum += *b * iota[i];
             });
         }
@@ -83,7 +83,7 @@ fn verify_round(
 
     // combine subclaims chi_00 and chi_rlc
     let x = verifier.generate();
-    let y = verifier.generate();
+    let y = x * x;
     beta[0] *= x;
     beta.iter_mut().skip(1).for_each(|b| *b *= y);
     let expected_sum = beta[0] * chi_00 + y * chi_rlc;
@@ -106,8 +106,8 @@ fn verify_round(
 
     // combine subclaims on rho
     let mut expected_sum = Fr::zero();
-    beta.iter_mut().enumerate().for_each(|(i, b)| {
-        *b = verifier.generate();
+    generate_beta(verifier, beta);
+    beta.iter().enumerate().for_each(|(i, b)| {
         expected_sum += *b * rho[i];
     });
 
@@ -124,8 +124,8 @@ fn verify_round(
     let theta_xor_base = theta.iter().map(|x| Fr::one() - x - x).collect::<Vec<_>>();
     let mut expected_sum = Fr::zero();
     // we'll need this beta to combine with the last theta sumcheck!
-    beta.iter_mut().enumerate().for_each(|(i, b)| {
-        *b = verifier.generate();
+    generate_beta(verifier, beta);
+    beta.iter().enumerate().for_each(|(i, b)| {
         expected_sum += *b * theta_xor_base[i];
     });
 
@@ -143,8 +143,8 @@ fn verify_round(
     // combine subclaims on theta d
     let mut expected_sum = Fr::zero();
     let mut beta_d = [Fr::zero(); 5];
-    beta_d.iter_mut().enumerate().for_each(|(i, b)| {
-        *b = verifier.generate();
+    generate_beta(verifier, &mut beta_d);
+    beta_d.iter().enumerate().for_each(|(i, b)| {
         expected_sum += *b * d[i];
     });
 
@@ -164,12 +164,13 @@ fn verify_round(
     let mut expected_sum = Fr::zero();
     let mut beta_c = [Fr::zero(); 5];
     let mut beta_rot_c = [Fr::zero(); 5];
-    beta_c.iter_mut().enumerate().for_each(|(i, b)| {
-        *b = verifier.generate();
+
+    generate_beta(verifier, &mut beta_c);
+    beta_c.iter().enumerate().for_each(|(i, b)| {
         expected_sum += *b * c[i];
     });
-    beta_rot_c.iter_mut().enumerate().for_each(|(i, b)| {
-        *b = verifier.generate();
+    generate_beta(verifier, &mut beta_rot_c);
+    beta_rot_c.iter().enumerate().for_each(|(i, b)| {
         expected_sum += *b * rot_c[i];
     });
 
@@ -194,15 +195,17 @@ fn verify_round(
     let mut expected_sum = Fr::zero();
     let mut beta_a = vec![Fr::zero(); a.len()];
 
+    let mut b = vec![Fr::zero(); 5];
+    generate_beta(verifier, &mut b);
     ai.iter().enumerate().for_each(|(i, a)| {
-        let b = verifier.generate();
         for j in 0..5 {
-            beta[j * 5 + i] *= b;
+            beta[j * 5 + i] *= b[i];
         }
-        expected_sum += b * *a;
+        expected_sum += b[i] * *a;
     });
-    beta_a.iter_mut().enumerate().for_each(|(i, b)| {
-        *b = verifier.generate();
+
+    generate_beta(verifier, &mut beta_a);
+    beta_a.iter().enumerate().for_each(|(i, b)| {
         expected_sum += *b * a[i];
     });
 
@@ -222,4 +225,14 @@ fn verify_round(
     iota.iter_mut().for_each(|i| *i = HALF * (Fr::one() - *i));
 
     (vrs_a, iota)
+}
+
+fn generate_beta(verifier: &mut Verifier, slice: &mut [Fr]) {
+    let base: Fr = verifier.generate();
+    let mut power = base;
+
+    slice.iter_mut().for_each(|x| {
+        *x = power;
+        power *= base;
+    });
 }
